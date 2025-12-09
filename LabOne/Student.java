bd90a99446685fe4f0df756d7e8830d641dcc177
MahirE251030 <jsjaber43214@gmail.com>
2025-12-09 20:58:50 +0600

    Create  Student.java

---
+++ LabOne/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    String name;
+    String id;
+    int age;
+    String email;
+    String phone;
+    String addreass;
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.id = "E251030";
+        s1.name = "Mohammed Mahir Asef";
+        s1.age = 21;
+        s1.email = "Mahir08@gmail.com";
+        s1.phone = "+8801764587452";
+        s1.address = "Dc road, west bakalia, mia baper mosjid";
+
+        System.out.println("Name: " + s1.name);
+        System.out.println("ID: " + s1.id);
+        System.out.println("Age: " + s1.age);
+        System.out.println("Email: " + s1.email);
+        System.out.println("Phone: " + s1.phone);
+        System.out.println("Address: " + s1.address);
+    }
+}
+
