30b2e2390db08fc846a9e2a5f3ba6cd07f2623aa
MahirE251030 <jsjaber43214@gmail.com>
2025-12-09 21:34:17 +0600

    Create  IDCard.java

---
+++ LabFour/IDCard.java
@@ -0,0 +1,28 @@
+class IDCard {
+    String name;
+    String id;
+    String department;
+    String institution;
+
+    IDCard(String name, String id, String department, String institution) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.institution = institution;
+    }
+
+    void showID() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Department: " + department);
+        System.out.println("Institution: " + institution);
+    }
+
+    public static void main(String[] args) {
+    
+        IDCard myID = new IDCard("Mohammed Mahir Asef", "E251030", "CCE", "International Islamic University Chittagong");
+
+        myID.showID();
+    }
+}
+
